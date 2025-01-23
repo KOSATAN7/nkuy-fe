6d7709427ce09018db5bf9aa030f9e3b2295bc17
Rifqi1012 <rifqimh1210@gmail.com>
2025-01-23 07:19:02 +0700

    [Update]: Gatau Apa wkwkwkwkwk

---
+++ src/components/Field/SelectField.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+interface SelectFieldProps {
+  options: { value: string; label: string }[];
+  value?: string;
+  onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  placeholder?: string;
+  name?: string;
+  className?: string;
+  title: string;
+}
+
+const SelectField: React.FC<SelectFieldProps> = ({
+  options,
+  value,
+  onChange,
+  placeholder = "Pilih salah satu",
+  name,
+  title,
+  className = "",
+}) => {
+  return (
+    <div>
+      <h1 className="mb-2">{title}</h1>
+      <div className={`w-full border-2 rounded-xl px-4 py-2 ${className}`}>
+        <select
+          name={name}
+          value={value}
+          onChange={onChange}
+          className="w-full focus:outline-none bg-transparent"
+        >
+          {placeholder && (
+            <option value="" disabled>
+              {placeholder}
+            </option>
+          )}
+          {options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default SelectField;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,19 @@
+import PhoneField from "@/components/Field/PhoneField";
+import SelectField from "@/components/Field/SelectField";
 import TextField from "@/components/Field/TextField";
+
+const options = [
+  { value: "apple", label: "Apple" },
+  { value: "banana", label: "Banana" },
+  { value: "cherry", label: "Cherry" },
+];
 
 const Dashboard = () => {
   return (
     <div className="">
         <TextField title="Kuda Liar" />
+        <PhoneField  />
+        <SelectField options={options} title="Kuda Kakak"  />
     </div>
   )
 }
